f66fc2219243380c740ab4ace133792e7d410d2a
pamo0827 <yoshi0827101328@gmail.com>
2025-05-30 15:22:51 +0900

    login_home

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,47 +1,46 @@
-class SessionsController < ApplicationController
-def create
-  unless request.env["omniauth.auth"][:uid]
-    flash[:danger] = "連携に失敗しました"
-    redirect_to root_url and return
-  end
-
-  user_data = request.env["omniauth.auth"]
-  user = User.find_by(uid: user_data[:uid])
-
-  if user
-    log_in(user)
-    flash[:success] = "ログインしました"
+class SessionsController < ApplicationController 
+  def create 
+    unless request.env["omniauth.auth"][:uid] 
+      flash[:danger] = "連携に失敗しました" 
+      redirect_to root_url and return 
+    end 
+   
+    user_data = request.env["omniauth.auth"] 
+    user = User.find_by(uid: user_data[:uid]) 
+   
+    if user 
+      log_in(user) 
+      flash[:success] = "ログインしました" 
+      redirect_to player_page_path  # 修正
+   
+    else 
+      new_user = User.new( 
+        uid:      user_data[:uid], 
+        nickname: user_data[:info][:nickname], 
+        name:     user_data[:info][:name], 
+        image:    user_data[:info][:image] 
+      ) 
+   
+      if new_user.save 
+        log_in(new_user) 
+        flash[:success] = "ユーザー登録成功" 
+        redirect_to player_page_path  # 修正
+      else 
+        flash[:danger] = "予期せぬエラーが発生しました" 
+        redirect_to root_url
+      end 
+    end 
+  end 
+  
+  def destroy 
+    log_out if logged_in? 
+    flash[:success] = "ログアウトしました" 
     redirect_to root_url
-
-  else
-    new_user = User.new(
-      uid:      user_data[:uid],
-      nickname: user_data[:info][:nickname],
-      name:     user_data[:info][:name],
-      image:    user_data[:info][:image]
-    )
-
-    if new_user.save
-      log_in(new_user)
-      flash[:success] = "ユーザー登録成功"
-    else
-      flash[:danger] = "予期せぬエラーが発生しました"
-    end
-    redirect_to root_url
-  end
-end
-
-def destroy
-  log_out if logged_in?
-  flash[:success] = "ログアウトしました"
-  redirect_to root_url
-end
-
-#ログイン処理
-def login
-  if logged_in?
-    redirect_to player_path
-  end
-end
-
+  end 
+  
+  def login 
+    if logged_in? 
+      redirect_to player_page_path  # 修正
+    end 
+  end 
 end
