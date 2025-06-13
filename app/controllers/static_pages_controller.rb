c0be459022ac76aad4972cf6ae6f1b2d0e495b8c
pamo0827 <yoshi0827101328@gmail.com>
2025-06-13 16:01:05 +0900

    プレイヤーコントローラーと静的ページコントローラーの修正: Spotifyセッションの確認と位置情報の追加、ホームページのログインリンク条件を修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,17 +1,15 @@
 require "rspotify"
 class StaticPagesController < ApplicationController
   def home
-    if logged_in? && session[:spotify_user_data]
-      access_token = session[:spotify_user_data]["credentials"]["token"]
-      user = RSpotify::User.new(session[:spotify_user_data])
-
-      playlists = user.playlists
-      if playlists.any?
-        first_playlist = playlists.first
-        first_track = first_playlist.tracks(limit: 1).first
-        @track_uri = first_track.uri if first_track
+    if logged_in?
+      if session[:spotify_user_data]
+        redirect_to player_page_path
+      else
+        # Spotifyセッションがない場合は、ログアウトして再認証を促す
+        log_out
+        session.delete(:spotify_user_data)
+        flash[:warning] = "Spotifyとの連携が必要です。再度ログインしてください。"
       end
-      redirect_to player_page_path and return
     end
   end
 end
