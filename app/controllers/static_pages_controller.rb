119bd515c4981631a891b65e05e46e0d182ea82a
GYact <yukito.go.work@gmail.com>
2025-06-18 13:04:10 +0900

    wip

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 require "rspotify"
 class StaticPagesController < ApplicationController
   def home
+    @hide_header = params[:logged_out] == 'true'
     if logged_in?
       if session[:spotify_user_data]
         redirect_to player_page_path
