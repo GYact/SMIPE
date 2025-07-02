22374bebbb8822424dd1be5f4929f6fcb5a49374
yachi <yachi200489@icloud.com>
2025-07-02 13:13:34 +0900

    models

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_25_054116) do
-  create_table "locations", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.float "latitude"
-    t.float "longitude"
-    t.string "location_name"
-    t.datetime "last_updated_at"
+ActiveRecord::Schema[8.0].define(version: 2025_06_27_084428) do
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_locations_on_user_id"
   end
 
   create_table "playlist_locations", force: :cascade do |t|
@@ -34,43 +29,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_25_054116) do
     t.index ["user_id"], name: "index_playlist_locations_on_user_id"
   end
 
-  create_table "playlist_tracks", force: :cascade do |t|
-    t.integer "playlist_id", null: false
-    t.integer "track_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["playlist_id", "track_id"], name: "index_playlist_tracks_on_playlist_id_and_track_id", unique: true
-    t.index ["playlist_id"], name: "index_playlist_tracks_on_playlist_id"
-    t.index ["track_id"], name: "index_playlist_tracks_on_track_id"
-  end
-
-  create_table "playlists", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_playlists_on_user_id"
-  end
-
-  create_table "shared_playlists", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "playlist_id"
-    t.float "latitude"
-    t.float "longitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_shared_playlists_on_user_id"
-  end
-
-  create_table "tracks", force: :cascade do |t|
-    t.string "name"
-    t.integer "duration_seconds"
-    t.integer "artist_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["artist_id"], name: "index_tracks_on_artist_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "uid"
     t.string "nickname"
@@ -78,16 +36,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_25_054116) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email"
-    t.float "latitude"
-    t.float "longitude"
+    t.decimal "latitude"
+    t.decimal "longitude"
+    t.string "location_name"
+    t.datetime "last_location_update"
   end
 
-  add_foreign_key "locations", "users"
   add_foreign_key "playlist_locations", "users"
-  add_foreign_key "playlist_tracks", "playlists"
-  add_foreign_key "playlist_tracks", "tracks"
-  add_foreign_key "playlists", "users"
-  add_foreign_key "shared_playlists", "users"
-  add_foreign_key "tracks", "artists"
 end
