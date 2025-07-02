3f3df9b8c8073472fb0a8f16ad010dcabf48683b
pamo0827 <yoshi0827101328@gmail.com>
2025-07-02 14:53:05 +0900

    Fix: Update Spotify callback URL to use environment variable

--- config/initializers/omuniauth.rb
+++ config/initializers/omuniauth.rb
@@ -2,6 +2,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :spotify, ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'],
            scope: 'user-read-private user-read-email playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public streaming user-modify-playback-state user-read-playback-state user-read-currently-playing user-library-read user-library-modify',
-           callback_url: "http://127.0.0.1:3000/auth/spotify/callback"
+           callback_url: ENV['SPOTIFY_CALLBACK_URL']
 end
 OmniAuth.config.allowed_request_methods = [:post, :get] 
