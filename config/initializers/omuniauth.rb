f6611d3e47cb2515cfeb17397f8e83ab054d8c5e
pamo0827 <yoshi0827101328@gmail.com>
2025-07-04 17:10:39 +0900

    Fix: Update Spotify callback URL handling to use environment-specific URLs

--- config/initializers/omuniauth.rb
+++ config/initializers/omuniauth.rb
@@ -1,11 +1,17 @@
-# config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'],
+  callback_url = if Rails.env.production?
+                   'https://smipe-7fuz.onrender.com/auth/spotify/callback'
+                 else
+                   'http://localhost:3000/auth/spotify/callback'
+                 end
+
+  provider :spotify,
+           ENV['SPOTIFY_CLIENT_ID'],
+           ENV['SPOTIFY_CLIENT_SECRET'],
            scope: 'user-read-private user-read-email playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public streaming user-modify-playback-state user-read-playback-state user-read-currently-playing user-library-read user-library-modify',
-           callback_url: ENV['SPOTIFY_CALLBACK_URL']
+           callback_url: callback_url
 end
 
-# 失敗時の処理
 OmniAuth.config.on_failure = proc { |env|
   message_key = env['omniauth.error.type']
   error_description = env['omniauth.error']&.error_reason
@@ -13,5 +19,4 @@ OmniAuth.config.on_failure = proc { |env|
   [302, {'Location' => new_path, 'Content-Type'=> 'text/plain'}, []]
 }
 
-# エラーハンドリング
-OmniAuth.config.logger = Rails.logger 
+OmniAuth.config.logger = Rails.logger
