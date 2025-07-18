50dbd7cad3c938dfdd3478e52ad5070dae785d16
pamo0827 <yoshi0827101328@gmail.com>
2025-07-18 17:34:46 +0900

    refactor: Remove unused playlist_spots and solid_cable_messages tables
    This commit removes the  table from  and the
     table from . These tables are no
    longer needed in the application.

--- db/cable_schema.rb
+++ db/cable_schema.rb
@@ -49,16 +49,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_09_053000) do
     t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
-  create_table "solid_cable_messages", force: :cascade do |t|
-    t.binary "channel", limit: 1024, null: false
-    t.binary "payload", limit: 536870912, null: false
-    t.datetime "created_at", null: false
-    t.integer "channel_hash", limit: 8, null: false
-    t.index ["channel"], name: "index_solid_cable_messages_on_channel"
-    t.index ["channel_hash"], name: "index_solid_cable_messages_on_channel_hash"
-    t.index ["created_at"], name: "index_solid_cable_messages_on_created_at"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "uid"
     t.string "nickname"
